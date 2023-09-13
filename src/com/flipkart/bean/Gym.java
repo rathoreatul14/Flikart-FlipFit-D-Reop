48b560624d208da8d504584231e75628ac10ae9c
KamakshiGoyal <kamakshi.goyal9@gmail.com>
2023-09-13 15:02:52 +0530

    Adding bean classes

--- src/com/flipkart/bean/Gym.java
+++ src/com/flipkart/bean/Gym.java
@@ -1,7 +1,45 @@
 package com.flipkart.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gym {
 
-	private int gymId;
-	
+	private int gymID;
+	private List<Integer> slotIDs = new ArrayList();
+	private String registerStatus;
+	private int gymOwnerID;
+
+	public int getGymID() {
+		return gymID;
+	}
+
+	public void setGymID(int gymID) {
+		this.gymID = gymID;
+	}
+
+	public List<Integer> getSlotIDs() {
+		return slotIDs;
+	}
+
+	public void setSlotIDs(List<Integer> slotIDs) {
+		this.slotIDs = slotIDs;
+	}
+
+	public String getRegisterStatus() {
+		return registerStatus;
+	}
+
+	public void setRegisterStatus(String registerStatus) {
+		this.registerStatus = registerStatus;
+	}
+
+	public int getGymOwner() {
+		return gymOwnerID;
+	}
+
+	public void setGymOwner(int gymOwnerID) {
+		this.gymOwnerID = gymOwnerID;
+	}
+
 }
---
+++ src/com/flipkart/bean/User.java
@@ -0,0 +1,59 @@
+package com.flipkart.bean;
+
+public class User {
+	private int userID;
+	private String name;
+	private String address;
+	private String email;
+	private String role;
+	private String password;
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
