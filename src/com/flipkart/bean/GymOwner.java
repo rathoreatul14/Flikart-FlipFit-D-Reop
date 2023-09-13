ac0c1cab9f8a36c502de075328f01b5d18391876
Saikalyan19 <126493602+Saikalyan19@users.noreply.github.com>
2023-09-13 16:04:57 +0530

    Update GymOwner.java

--- src/com/flipkart/bean/GymOwner.java
+++ src/com/flipkart/bean/GymOwner.java
@@ -1,4 +1,5 @@
 package com.flipkart.bean;
+
 /**
  * @author msaikalyan.yadav
  */
@@ -6,26 +7,56 @@ public class GymOwner {
 	private int id;
 	private String name;
 	private String status;
+	private String aadharNumber;
+	private String contactNumber;
+	private String address;
+
+	public String getAadharNumber() {
+		return aadharNumber;
+	}
+
+	public void setAadharNumber(String aadharNumber) {
+		this.aadharNumber = aadharNumber;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	
-	
-	
 
 }
