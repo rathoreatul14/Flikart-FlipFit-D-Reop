30d0e07f2637bd0b810ffe2a91dcda11ef2a7e54
Atul Rathore <atul.rathore@BLRETV-QHYJ9K3V.local>
2023-09-13 14:42:28 +0530

    Adding Slot bean

--- src/com/flipkart/bean/Slot.java
+++ src/com/flipkart/bean/Slot.java
@@ -1,5 +1,59 @@
 package com.flipkart.bean;
 
-public class Slot {
+import jdk.jfr.Timestamp;
 
+public class Slot {
+    private int slotId;
+    private int gymId;
+    private Timestamp startTime;
+    private int numberOfSeats;
+    private int availableSeats;
+
+    public Slot(int slotId, int gymId, Timestamp startTime, int numberOfSeats, int availableSeats) {
+        this.slotId = slotId;
+        this.gymId = gymId;
+        this.startTime = startTime;
+        this.numberOfSeats = numberOfSeats;
+        this.availableSeats = availableSeats;
+    }
+
+    public int getSlotId() {
+        return slotId;
+    }
+
+    public void setSlotId(int slotId) {
+        this.slotId = slotId;
+    }
+
+    public int getGymId() {
+        return gymId;
+    }
+
+    public void setGymId(int gymId) {
+        this.gymId = gymId;
+    }
+
+    public Timestamp getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
 }
