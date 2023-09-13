5b46f2d50134aeca95b6aeac8e1189422cb075f5
KamakshiGoyal <kamakshi.goyal9@gmail.com>
2023-09-13 16:02:03 +0530

    Adding Bean Class

---
+++ src/com/flipkart/bean/Notification.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.flipkart.bean;
+
+/**
+ * @author kamakshi.goyal
+ */
+public class Notification {
+
+	private int notificationID;
+	private String notificationTitle;
+	private String notificationMessage;
+
+	public int getNotificationID() {
+		return notificationID;
+	}
+
+	public void setNotificationID(int notificationID) {
+		this.notificationID = notificationID;
+	}
+
+	public String getNotificationTitle() {
+		return notificationTitle;
+	}
+
+	public void setNotificationTitle(String notificationTitle) {
+		this.notificationTitle = notificationTitle;
+	}
+
+	public String getNotificationMessage() {
+		return notificationMessage;
+	}
+
+	public void setNotificationMessage(String notificationMessage) {
+		this.notificationMessage = notificationMessage;
+	}
+
+}
