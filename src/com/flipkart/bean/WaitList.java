ee4a26a8710227e8eee685b3c3fcc2fe05ffc55c
Atul Rathore <atul.rathore@BLRETV-QHYJ9K3V.local>
2023-09-13 14:50:32 +0530

    Adding Waitlist bean

--- src/com/flipkart/bean/WaitList.java
+++ src/com/flipkart/bean/WaitList.java
@@ -1,5 +1,37 @@
 package com.flipkart.bean;
 
 public class WaitList {
+    private int userId;
+    private int gymId;
+    private int slotId;
 
+    public WaitList(int userId, int gymId, int slotId) {
+        this.userId = userId;
+        this.gymId = gymId;
+        this.slotId = slotId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getGymId() {
+        return gymId;
+    }
+
+    public void setGymId(int gymId) {
+        this.gymId = gymId;
+    }
+
+    public int getSlotId() {
+        return slotId;
+    }
+
+    public void setSlotId(int slotId) {
+        this.slotId = slotId;
+    }
 }
