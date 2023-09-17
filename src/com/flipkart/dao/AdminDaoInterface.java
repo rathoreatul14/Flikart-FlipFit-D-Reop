095cf613b35c7328e4cbf29491466d7a476de902
Atul Rathore <atul.rathore@BLRETV-QHYJ9K3V.local>
2023-09-17 13:59:16 +0530

    Adding admin interface

--- src/com/flipkart/dao/AdminDaoInterface.java
+++ src/com/flipkart/dao/AdminDaoInterface.java
@@ -7,5 +7,21 @@ package com.flipkart.dao;
  * 
  */
 public interface AdminDaoInterface {
+
+	void fetchProfile(String userName);
+
+	void updatePassword(String userName, String Password);
+
+	void viewAllGyms();
+
+	void viewAllGymOwners();
+
+	void approveGymOwner(int id);
+
+	void approveGym(int id);
+
+	void viewPendingGymOwner();
+
+	void viewPendingGym();
 	
 }
