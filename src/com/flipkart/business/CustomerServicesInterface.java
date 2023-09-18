dbb503fd313bdfb2df3a28b1626e2e6e3fd723d0
rathoreatul14 <127722608+rathoreatul14@users.noreply.github.com>
2023-09-18 14:41:54 +0530

    Update CustomerServicesInterface.java
    Added comments

--- src/com/flipkart/business/CustomerServicesInterface.java
+++ src/com/flipkart/business/CustomerServicesInterface.java
@@ -10,17 +10,28 @@ import com.flipkart.bean.Customer;
  * @author arjit.giri
  */
 public interface CustomerServicesInterface {
-	
+
+	// This method is used to register a new customer
+	// @params Scanner
 	void registerCustomer(Scanner sc);
 
+	// This method is used to view the details of a customer
+	// @params Customer
 	void viewProfile(Customer customer);
 
+	// This method is used to update details of a customer
+	// @params Customer
 	void updateProfile(Customer customer);
 
+	// This method is used by customer to book a slot
+	// @params Customer
 	void bookSlot(Customer customer);
 
+	// This method is used to view the details of all gyms
 	void viewGyms();
 
+	// This method is used to view the bookings of a customer
+	// @params Customer
 	void viewBookings(Customer customer);
 	
 
