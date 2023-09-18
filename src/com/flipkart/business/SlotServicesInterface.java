2c8d956824ce4dd0e2f0c10c4e346b0c23ff2296
rathoreatul14 <127722608+rathoreatul14@users.noreply.github.com>
2023-09-18 14:58:20 +0530

    Update SlotServicesInterface.java

--- src/com/flipkart/business/SlotServicesInterface.java
+++ src/com/flipkart/business/SlotServicesInterface.java
@@ -9,8 +9,13 @@ import com.flipkart.bean.Slot;
  * @author arjit.giri
  */
 public interface SlotServicesInterface {
-	
+
+	// This method is used to add a new slot
+	//@params gymOwnerId
 	public void addSlot(int gymOwnerId);
+
+	// This method is used to delete a slot
+	// @params slotId
 	public void deleteSlot(int slotId);
 	
 
