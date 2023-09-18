20fae622e03ed255806e1f3a340581b348382baa
rathoreatul14 <127722608+rathoreatul14@users.noreply.github.com>
2023-09-18 14:55:12 +0530

    Update GymOwnerServicesInterface.java
    Added comments

--- src/com/flipkart/business/GymOwnerServicesInterface.java
+++ src/com/flipkart/business/GymOwnerServicesInterface.java
@@ -11,14 +11,25 @@ import com.flipkart.bean.GymOwner;
  * 
  */
 public interface GymOwnerServicesInterface {
-	
+
+	// This method is used to view profile details of gym owner
+	// @params GymOwner
 	public void viewProfile(GymOwner owner);
-	
+
+	// This method is used to update gym owner profile
+	// @params GymOwner
 	public void updateProfile(GymOwner owner);
-	
+
+	// This method is used to register a gym owner
+	// @params Scanner
 	public void registerGymOwner(Scanner in);
-	
+
+	// This method is used to view all gyms of a particular owner
+	// @params GymOwner
 	public void viewGyms(GymOwner owner);
 
+	// This method is used to register a new gym
+	// @params Scanner
+	// @params GymOwner
 	public void registerGym(Scanner in, GymOwner owner);
 }
