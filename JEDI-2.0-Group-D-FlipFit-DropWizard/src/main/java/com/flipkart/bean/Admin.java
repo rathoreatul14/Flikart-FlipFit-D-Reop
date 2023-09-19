0727c0ea66948a2785f8fa5e89e117d04d755d02
rathoreatul14 <127722608+rathoreatul14@users.noreply.github.com>
2023-09-19 15:38:05 +0530

    Update Admin.java

--- JEDI-2.0-Group-D-FlipFit-DropWizard/src/main/java/com/flipkart/bean/Admin.java
+++ JEDI-2.0-Group-D-FlipFit-DropWizard/src/main/java/com/flipkart/bean/Admin.java
@@ -1,63 +1,23 @@
-package com.flipkart.controller;
+package com.flipkart.bean;
 
-import com.flipkart.bean.Gym;
-import com.flipkart.service.AdminServices;
-import com.flipkart.service.AdminServicesInterface;
-import com.flipkart.bean.GymOwner;
-import com.flipkart.service.*;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.PUT;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+public class Admin {
+	private int adminId;
+	private String name;
 
-import java.util.ArrayList;
-import java.util.List;
+	public int getAdminId() {
+		return adminId;
+	}
 
-@Path("/admin")
-public class AdminController {
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
 
-    private final AdminServices adminService = new AdminServices();
+	public String getName() {
+		return name;
+	}
 
-    @GET
-    @Path("/gyms")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<Gym> viewAllGyms(){
-        return adminService.viewAllGyms();
-    }
-
-    @GET
-    @Path("/gymOwners")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<GymOwner> viewAllGymOwners(){
-        return adminService.viewAllGymOwners();
-    }
-
-    @GET
-    @Path("/pending/gyms")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<Gym> viewPendingGyms(){
-        return adminService.viewPendingGym();
-    }
-
-    @GET
-    @Path("/pending/gymOwners")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<GymOwner> viewPendingGymOwners(){
-        return adminService.viewPendingGymOwner();
-    }
-
-    @PUT
-    @Path("/approve/gyms")
-    public String approveGyms(){
-        return "Gyms";
-    }
-
-    @PUT
-    @Path("/approve/gymOwners")
-    public String approveGymOwners(){
-        return "Gyms";
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
